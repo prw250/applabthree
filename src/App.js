2cc9ea24efc904b099e11730afd2d608a3c65f0a
Patricia <prw250@nyu.edu>
2019-09-18 12:02:30 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>All About Lab Three</h1>
+   <img width="500"alt="lab three office"src="https://yt3.ggpht.com/a-/AAuE7mBZItqw5ydjYGJGKi5xbfKer8hN64xKbr7Fhw=s900-mo-c-c0xffffffff-rj-k-no"/>
+   <p>
+     When I first looked up Lab Three, I found it was not only the name of our lab, but also the name of a co-working space.
+   </p>
+   <img className="blue-border"width="500"alt="lab three office"src="https://www.labthree.com.br/images/full-project-6.jpg"/> 
+   <p>This co-working space seems to have a nature vibe while also trying to be modern.</p>
     </div>
   );
 }
