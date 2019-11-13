904895936044b1fd3b34640fcd08c51f5909f028
Patricia <prw250@nyu.edu>
2019-11-13 12:57:35 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import './App.css';
+ 
+let post =[{
+  date:
+  content:
+},{
+  date:
+  content:
+}
+
+]
 
 function App() {
   return (
-    <div className="App">
-    <h1>All About Lab Three</h1>
-   <img width="500"alt="lab three office"src="https://yt3.ggpht.com/a-/AAuE7mBZItqw5ydjYGJGKi5xbfKer8hN64xKbr7Fhw=s900-mo-c-c0xffffffff-rj-k-no"/>
-   <p>
-     When I first looked up Lab Three, I found it was not only the name of our lab, but also the name of a co-working space.
-   </p>
-   <img className="blue-border"width="500"alt="lab three office"src="https://www.labthree.com.br/images/full-project-6.jpg"/> 
-   <p>This co-working space seems to have a nature vibe while also trying to be modern.</p>
+    <div>
+  <Profile content={post[0].content} image={post[0].image} alt ={post[0].alt}/>
+  <Profile content={post[1].content} image={post[1].image} alt ={post[1].alt}/>
     </div>
   );
 }
 
+function Profile(props) {
+  return (
+    <div className="profileCard">
+      <div className="profileImage" />
+      <div className="profileHeader">
+        <div>
+          <h1>{props.date}</h1>
+          <p>Columbia River, OR</p>
+        </div>
+        <div className="likes">
+          <h3>100 likes</h3>
+        </div>
+
+      </div>
+      <div className="profileDescription">
+        <img src={props.image} alt={props.alt} />
+        <p>{props.content}</p>
+      </div>
+    </div>
+  )
+}
 export default App;
